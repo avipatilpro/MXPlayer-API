d7a29fc5bfe746ceffbf68084172a19231ff8808
Avishkar Patil <avishkarpatil29@gmail.com>
2021-07-03 20:09:07 +0530

    Initial Commit

--- index.php
+++ index.php
@@ -25,16 +25,112 @@ $mdes =$mxdata['description'];
 $relese =$mxdata['releaseDate'];
 $imgu =$mxdata['imageInfo'][1]['url'];
 $img = "https://isa-1.mxplay.com/".$imgu;
+$vhash = $mxdata['stream']['videoHash'];
 
-$hls =$mxdata['stream']['hls']['high'];
-$dash =$mxdata['stream']['dash']['high'];
-$hlst =$mxdata['stream']['thirdParty']['hlsUrl'];
-$dasht =$mxdata['stream']['thirdParty']['dashUrl'];
-$hurl = "https://llvod.mxplay.com/".$hls;
-$durl = "https://llvod.mxplay.com/".$dash;
+$dasht = $mxdata['stream']['thirdParty']['dashUrl'];
+$alt = $mxdata['stream']['altBalaji']['dashUrl'];
+$h = $mxdata['stream']['dash']['high'];
+$b = $mxdata['stream']['dash']['base'];
+$m = $mxdata['stream']['dash']['main'];
 
 
- $apii = array("id" => $id, "type" => $type, "title" => $mtitle, "description" => $mdes, "releaseDate" => $relese, "images" => $img, "hls" => $hurl, "dash" => $durl, "hlsUrl" => $hlst, "dashUrl" => $dasht);
+$hu = $mxdata['stream']['thirdParty']['hlsUrl'];
+$ab = $mxdata['stream']['altBalaji']['hlsUrl'];
+$x = $mxdata['stream']['hls']['high'];
+$y = $mxdata['stream']['hls']['base'];
+$z = $mxdata['stream']['hls']['main'];
+
+ 
+//--------- DONT THINK ABOYT THIS ------//
+
+if (empty($h)) {
+}
+else
+{$dash=$h;}
+  if (empty($b)) {
+}
+  else
+{$dash=$b;}
+
+  if (empty($m)) {
+}
+else
+{$dash=$m;}
+
+
+if (empty($alt)) {
+}
+else
+{$dd=$alt;}
+
+if (empty($dasht)) {
+}
+else
+{$dd=$dasht;}
+
+
+if (preg_match("/https/", $dash))
+{$url = $dash;}
+else
+{$url = "https://llvod.mxplay.com/" . $dash;}
+
+if (is_null($dash)){
+$dplay = $dd;
+}
+ else
+ {$dplay = $url;}
+
+// Final OutPut Is $dplay ( DRM URL)
+
+
+if (empty($x)) {
+}
+else
+{$hls =$x;}
+  if (empty($y)) {
+}
+  else
+{$hls=$y;}
+
+  if (empty($z)) {
+}
+else
+{$hls=$z;}
+
+
+if (empty($ab)) {
+}
+else
+{$hh=$ab;}
+
+if (empty($hu)) {
+  }
+else
+{$hh=$hu;}
+
+
+
+if (preg_match("/https/", $hls))
+{$xyz = $hls;}
+else
+{$xyz = "https://llvod.mxplay.com/" . $hls;}
+ 
+if (is_null($hls))
+{$hplay = $hh;}
+else
+{ $hplay = $xyz;}
+
+
+
+// Fianl output is $hplay (HLS URL)
+
+
+ 
+ //--------- DONT THINK ABOYT THIS ------//
+ 
+ 
+
+ $apii = array("id" => $id, "type" => $type, "title" => $mtitle, "description" => $mdes, "releaseDate" => $relese, "images" => $img, "videoHash" => $vhash, "hls" => $hplay, "dash" => $dplay);
 
  $api =json_encode($apii, JSON_UNESCAPED_SLASHES);
 
